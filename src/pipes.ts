475eb43916d547c7327ac42aa2a8c3681b663610
localusercamp <user062199@gmail.com>
2022-01-21 11:07:31 +0500

    trying things

--- src/pipes.ts
+++ src/pipes.ts
@@ -1,13 +1,15 @@
 import { starship } from "./core";
 
+
+
 /**
  * Пайп для массива
  * @param {Array} source Исходный массив
  */
-const ArrayPipe = (source: Array<any> = []) : ArrayPipeObject => ({
-  _source:  source,
-  _filters: [],
-  _sorts:   [],
+class ArrayPipe {
+  _source:  Array<any>      = [];
+  _filters: Array<Function> = [];
+  _sorts:   Array<Function> = [];
 
 
 
@@ -15,34 +17,49 @@ const ArrayPipe = (source: Array<any> = []) : ArrayPipeObject => ({
    * Добавляет новый фильтр в очередь фильтров
    * @param {Function} fn Фильтр
    */
-  filter(fn: Function) {
+  public filter(fn: Function) : this 
+  {
     this._filters.push(fn);
     return this;
-  },
+  }
+
+  /**
+   * Добавляет новую сортировку в очередь сортировок
+   * @param {Function} fn Сортировка
+   */
+  public sort(fn: Function) : this
+  {
+    this._sorts.push(fn);
+    return this;
+  }
+
+
 
   /**
    * Применяет фильтры к массиву
    * @param {Array} source Массив для модификации (если не указан то используется исходный массив)
    * @returns {Array} Модифицированный массив
    */
-  applyFilters(source = null) {
+  protected applyFilters(source: undefined|Array<any> = undefined) : Array<any>
+  {
     return this._filters.reduce(
       (heap, fn:any) => heap.filter(fn),
       source ?? this._source
     );
-  },
+  }
   
   /**
    * Применяет сортировки к массиву
    * @param {Array} source Массив для модификации (если не указан то используется исходный массив)
    * @returns {Array} Модифицированный массив
    */
-  applySorts(source = null) {
+  protected applySorts(source: undefined|Array<any> = undefined) : Array<any>
+  {
     return this._sorts.reduce(
       (heap, fn:any) => heap.sort(fn),
       source ?? this._source
     );
-  },
+  }
 
 
 
@@ -50,14 +67,13 @@ const ArrayPipe = (source: Array<any> = []) : ArrayPipeObject => ({
    * Модифицирует исходный массив
    * @returns {Array} Модифицированный исходный массив
    */
-  passthrough() {
-    return (() =>
-      this.applySorts(
-        this.applyFilters()
-      )
-    )();
-  },
-});
+  public passthrough() : Array<any>
+  {
+    return this.applySorts(
+      this.applyFilters()
+    );
+  }
+}
 
 
 
