a7bf72c8ab5269f4acdd83a89a02f091242655cb
localusercamp <user062199@gmail.com>
2022-01-25 14:50:18 +0500

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 export * from './ArrayPipe';
-
 export * from './function';
-
 export * from './number';
-
 export * from './special';
-
 export * from './type';
+
+export * as default from './function';
