2e80be143defc15f8a01de65a8472f2c9d10c42d
localusercamp <user062199@gmail.com>
2022-03-17 10:01:53 +0500

    More tokens added to simpleDateTimeFormat

--- src/datetime.ts
+++ src/datetime.ts
@@ -3,7 +3,7 @@ import { replaceMany } from './string';
 /**
  * Конвертирует дату в соответствии с форматом
  * @param value Дата для форматирования. Если передан не объект `Date` то будет выполнена попытка преобразовать значение
- * @param format Строка содержащая токены: `D M Y d m y`
+ * @param format Строка содержащая токены: `D M Y d m y H I S h i s`
  * @example 
  * simpleDateTimeFormat(new Date, 'D.M.Y') 
  * simpleDateTimeFormat(new Date, 'на D день')
@@ -19,7 +19,15 @@ const simpleDateTimeFormat = (value: string|number|Date, format: string): string
   const M = m.toString().padStart(2, '0');
   const y = Y.toString().substring(2);
 
-  return replaceMany(format, { D, d, M, m, Y, y });
+  const h = date.getHours();
+  const i = date.getMinutes();
+  const s = date.getSeconds();
+
+  const H = h.toString().padStart(2, '0');
+  const I = i.toString().padStart(2, '0');
+  const S = s.toString().padStart(2, '0');
+
+  return replaceMany(format, { D, d, M, m, Y, y, H, h, I, i, S, s });
 };
 
 
